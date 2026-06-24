20116c26be83b9c9a142e7aebbc6689d42440f41
hyeoksang kwon <gurtkd3380@naver.com>
2026-06-24 16:43:18 +0900

    Feat : 동물 클래스 구현

--- calculator.py
+++ calculator.py
@@ -1,21 +1,3 @@
-def add(a, b):
-    return a + b
-
-
-def multiply(a, b):
-    return a * b
-
-
-def subtract(a, b):
-    return a - b
-
-
-def divide(x, y):
-    if y == 0:
-        return None
-    return x / y
-
-
 class Greetings:
     def __init__(self, name):
         self.name = name
@@ -25,3 +7,31 @@ class Greetings:
 
     def new_friend(self, name):
         print(f"nice to meet you, {name} ~~!")
+
+
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sleep(self):
+        print(f"{self.name} is sleeping.")
+
+    def eat(self):
+        print(f"{self.name} is eating.")
+
+
+class Dog(Animal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def bark(self):
+        print("bark bark bark~~!")
+
+
+class Cat(Animal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def meow(self):
+        print("meow meow meow~~!")
